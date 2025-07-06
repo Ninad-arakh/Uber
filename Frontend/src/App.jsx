51b7806646c6d24a5bb503369a0896b05bc6296c
Ninad-arakh <arakhninad@gmail.com>
2025-07-06 15:16:55 +0530

    basic vite app with pages and configurations

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import UserSignup from "./pages/UserSignup";
+import UserLogin from "./pages/UserLogin";
+import CaptainSignup from "./pages/CaptainSignup";
+import CaptainLogin from "./pages/CaptainLogin";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user-signup" element={<UserSignup />} />
+        <Route path="/user-login" element={<UserLogin />} />
+        <Route path="/captain-signup" element={<CaptainSignup />} />
+        <Route path="/captain-login" element={<CaptainLogin />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div className="h-screen w-full bg-red-400">
+            <div className="">
+                <h2 className='text-3xl font-bold text-center'>Welcome to Uber</h2>
+                <button className="">
+                    Get Started
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
