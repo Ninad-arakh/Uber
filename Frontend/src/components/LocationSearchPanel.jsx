7d9bf6d1fd47ab63be1190cd0d02e5d2da513f80
Ninad-arakh <arakhninad@gmail.com>
2025-07-11 20:49:12 +0530

    daily push

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { locations } from "../constants/constants";
 
-const LocationSearchPanel = () => {
+const LocationSearchPanel = ({vehiclePanel, setVehiclePanel}) => {
   return (
-    <div>LocationSearchPanel</div>
-  )
-}
+    <div>
+      {locations.map((l) => {
+        return (
+          <div className="flex items-center gap-3 justify-start my-2 p-4 shadow-md rounded-lg">
+            <h2 className="bg-[#eee] h-10 flex justify-center w-10 rounded-full p-2">
+              <i className="text-xl ri-map-pin-line"></i>
+            </h2>
+            <h4 className="font-medium">{l}</h4>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default LocationSearchPanel
+export default LocationSearchPanel;
--- Frontend/src/constants/constants.js
+++ Frontend/src/constants/constants.js
@@ -1,1 +1,14 @@
 export const BASE_URL="http://localhost:3000/";
+
+export const locations = [
+  "123 Elm Street, Springfield, IL, USA",
+  "456 Maple Avenue, Toronto, ON, Canada",
+  "789 Oak Lane, London, England, UK",
+  "101 Pine Road, Sydney, NSW, Australia",
+  "202 Birch Boulevard, Paris, France",
+  "303 Cedar Drive, Berlin, Germany",
+  "404 Willow Way, Tokyo, Japan",
+  "505 Redwood Circle, Cape Town, South Africa",
+  "606 Fir Street, Buenos Aires, Argentina",
+  "707 Palm Parkway, Dubai, UAE"
+]
